b16b704a8758bee9a7b5ad378ab3baee42a37f22
leeseungjin <dltmdwls154@gmail.com>
2022-02-03 16:04:27 +0900

    book search 구현

--- src/components/axios/axiosGet.jsx
+++ src/components/axios/axiosGet.jsx
@@ -4,20 +4,63 @@ import axios from 'axios'
 
 
 const AxiosGet = () => {
-  const [data, setData] = useState();
-  useEffect(() => {
-    axios.get("https://dapi.kakao.com/v3/search/book?target=title",{
-      data:{query:'미움받을 용기'},
-      headers:{Authorization: 'KakaoAK f9b256f27ac94c60b3b60548d768b661'}
+  const [data, setData] = useState([]);
+  useEffect( () => {
+    axios.get("https://dapi.kakao.com/v3/search/book?target=title", {
+      params: {
+        query: '미움받을 용기'
+        //sort	String	결과 문서 정렬 방식, accuracy(정확도순) 또는 latest(발간일순), 기본값 accuracy	X
+        // page	Integer	결과 페이지 번호, 1~50 사이의 값, 기본 값 1	X
+        // size	Integer	한 페이지에 보여질 문서 수, 1~50 사이의 값, 기본 값 10	X
+        // target	String	검색 필드 제한
+        // 사용 가능한 값: title(제목), isbn (ISBN), publisher(출판사), person(인명)	X
+      },
+      headers: {
+        Authorization: 'KakaoAK 916ced9befd4ecf22b601f586949912d'
+      },
     })
-      .then(res => console.log(res))
+    .then(res =>
+      setData(res.data.documents)
+    )
+    .catch(err => console.log(err))
   }, []);
 
+  const onchange = (e) => {
+    e.target.value && axios.get("https://dapi.kakao.com/v3/search/book?target=title", {
+      params: {
+        query: e.target.value
+      },
+      headers: {
+        Authorization: 'KakaoAK 916ced9befd4ecf22b601f586949912d'
+      },
+    })
+      .then(res =>
+        setData(res.data.documents)
+      )
+      .catch(err => console.log(err))
+  }
   return (
     <div>
-
+      <input type="text" onChange={onchange}/>
+      {data.map((v,i) => {
+          return(
+            <div key={i}>
+              <h2>{v.title}</h2>
+              <Imgs src={v.thumbnail}/>
+              <div>{v.contents}</div>
+              <div>{v.price}원 저자 {v.authors.map(v => v)}</div>
+              <div>링크 <a href={v.url}>클릭</a></div>
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
+const Imgs = styled.img`
+  width: 200px;
+  height: 400px;
+`
 
 export default AxiosGet;
+
