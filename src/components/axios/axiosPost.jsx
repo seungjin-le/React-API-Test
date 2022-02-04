bfd45090e9ad866fac33613a38afbcae9d5ccd1f
LeeSeungJin <dltmdwls154@gmail.com>
2022-02-04 23:19:58 +0900

    pagination add

---
+++ src/components/axios/axiosPost.jsx
@@ -0,0 +1,39 @@
+import axios from 'axios'
+import {useState} from 'react'
+
+const AxiosPost = () => {
+  const [data, setData] = useState([]);
+
+  const getClick = () => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => setData(res.data))
+  }
+  const postClick = () => {
+    axios.post('https://jsonplaceholder.typicode.com/posts',{
+      userId :11,
+      id:101,
+      body:'test body',
+      title:'test title'
+    })
+      .then(res => console.log(res.data))
+  }
+  return (
+    <div>
+      <h2>API 연습</h2>
+      <div>
+        <button onClick={getClick}>Get</button>
+        <button onClick={postClick}>Post</button>
+      </div>
+      {data.map((v,i) => {
+        return (
+          <div key={i}>
+            <h3>{v.title}</h3>
+            <div>userId = {v.userId}, id = {v.id}</div>
+            <div>{v.body}</div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+export default AxiosPost
--- src/components/axios/index.jsx
+++ src/components/axios/index.jsx
@@ -1,42 +1,25 @@
-import AxiosGet from './axiosGet'
-import {useEffect, useState} from 'react'
-import axios from 'axios'
+
+import AxiosPost from './axiosPost'
+import Pagination from 'react-js-pagination'
+import {useState} from 'react'
+
 
 
 const AxiosPage = () => {
-  const [data, setData] = useState([]);
+  const handlePageChange = () => {
 
-  const getClick = () => {
-    axios.get('https://jsonplaceholder.typicode.com/posts')
-      .then(res => setData(res.data))
-    console.log(data)
-  }
-  const postClick = () => {
-    axios.post('https://jsonplaceholder.typicode.com/posts',{
-      userId :11,
-      id:101,
-      body:'test body',
-      title:'test title'
-    })
-      .then(res => console.log(res.data))
-  }
+  };
   return (
-    <div>
-      <h2>API 연습</h2>
-      <div>
-        <button onClick={getClick}>Get</button>
-        <button onClick={postClick}>Post</button>
-      </div>
-      {data.map((v,i) => {
-        return (
-          <div key={i}>
-            <h3>{v.title}</h3>
-            <div>userId = {v.userId}, id = {v.id}</div>
-            <div>{v.body}</div>
-          </div>
-        )
-      })}
-    </div>
+    <>
+      <Pagination
+        activePage={3}
+        itemsCountPerPage={10}
+        totalItemsCount={450}
+        pageRangeDisplayed={5}
+        onChange={handlePageChange}
+      />
+      <AxiosPost />
+    </>
   )
 }
 //<AxiosGet />
